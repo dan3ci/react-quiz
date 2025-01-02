512c9dde0c6c8ed3499efa945c57e937b8374d10
dan3ci <everyhomeaakash@gmail.com>
2025-01-02 13:30:25 +0545

    data fetching established

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import Header from "./Header";
+import { StrictMode, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Main from "./main";
 
 function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  }, []);
   return (
-    <>
+    <div className="app">
       <Header />
-    </>
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
   );
 }
 
 export default App;
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);

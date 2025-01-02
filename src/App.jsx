554070e3160d2f04bfc3fa95ba289f2b9c33f24b
dan3ci <everyhomeaakash@gmail.com>
2025-01-02 15:13:51 +0545

    quiz start UI completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { StrictMode, useEffect, useReducer } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Main from "./main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -29,7 +32,8 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -42,8 +46,9 @@ export default function App() {
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
